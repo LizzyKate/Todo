150bdc592404e9f6e3bb229416b4fa481861d331
bemijonathan <bemijonathan@gmail.com>
2020-03-31 16:30:06 +0100

    fix: i fixed the switch for dark and light mode

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,52 @@
 <template>
-  <div :class="change()">
-    <p> i am a boy</p>
-      <button type="button" class="btn btn-primary btn-lg" v-on:click="toggle=!toggle">Large button</button>
+  <div :class="currentClass" class="screenbody">
+    <p>i am a boy</p>
+    <button type="button" class="btn btn-primary btn-lg" v-on:click="change()">Large button</button>
   </div>
 </template>
 
 <script>
-import  './assets/main.css';
-
+import "./assets/main.css";
 
 export default {
-  data(){
-    return{
-      toggle:false,
-    }
+  data() {
+    return {
+      currentClass: "white"
+    };
   },
-   methods:{
-     change(){
-       document.body.className = this.toggle;
-       if (this.toggle === true){
-         document.body.style.backgroundColor = 'black'
-       } else {
-         document.body.style.removeProperty("background-color")
-       }
-      //  return {
-      //    toggle: this.toggle
+  methods: {
+    change() {
+      if (this.currentClass === "white") {
+        this.currentClass = "black";
+      } else {
+        this.currentClass = "white";
+      }
+      //  document.body.className = this.toggle;
+      //  if (this.toggle === true){
+      //    document.body.style.backgroundColor = 'black'
+      //  } else {
+      //    document.body.style.removeProperty("background-color")
       //  }
-     }
-   }
-  
-}
+    }
+  }
+};
 </script>
 
 <style>
+.black {
+  background: black;
+}
+.white {
+  background: white;
+}
+
+.black p {
+  color:blue;
+}
+.white p{
+  color: yellow;
+}
+.screenbody{
+  min-height:100vh ;
+}
 </style>
